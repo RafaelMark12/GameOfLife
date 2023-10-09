e90124673605108bb4a588521fd1cb7669af0afa
RafaelMark12 <rafael.mark1224@gmail.com>
2023-10-09 07:30:17 +0200

    Átjavítva a solution filet, hogy lehessen dolgozni.

---
+++ GameOfLife_SZFT6_CS3/Sejtek.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife_SZFT6_CS3
+{
+    internal class Sejtek
+    {
+        public int sejt { get; init; }
+        public Sejtek(int sejt)
+        {
+            this.sejt = sejt;
+        }
+
+    }
+}
