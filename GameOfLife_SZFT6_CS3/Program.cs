ca51d4e47430990c24ce85584c6849fbc2b805e5
RafaelMark12 <rafael.mark1224@gmail.com>
2023-10-09 14:48:01 +0200

    F1-F2 kialakítás

--- GameOfLife_SZFT6_CS3/Program.cs
+++ GameOfLife_SZFT6_CS3/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Teszt");
+﻿using GameOfLife_SZFT6_CS3;
+
+Sejtek mezo = new();
+
+Console.Clear();
+
+for (int i = 0; i < 40; i++)
+{
+    for (int j = 0; j < 40; j++)
+    {
+        Console.Write(mezo.mezo[i,j]);
+    }
+}
--- GameOfLife_SZFT6_CS3/Sejtek.cs
+++ GameOfLife_SZFT6_CS3/Sejtek.cs
@@ -8,11 +8,48 @@ namespace GameOfLife_SZFT6_CS3
 {
     internal class Sejtek
     {
-        public int sejt { get; init; }
-        public Sejtek(int sejt)
+        public int[,] mezo = new int[75, 75];
+        public ConsoleKeyInfo billentyukulcs = Console.ReadKey();
+
+
+        public Sejtek()
         {
-            this.sejt = sejt;
+            for (int i = 0; i < 75; i++)
+            {
+                for (int j = 0; j < 75; j++)
+                {
+                    mezo[i, j] = 0;
+                }
+            }
+
+            switch (billentyukulcs.KeyChar)
+            {
+                case '1':
+                    mezo[11, 11] = 1;
+                    mezo[11, 12] = 1;
+                    mezo[11, 11] = 1;
+                    mezo[11, 13] = 1;
+                    mezo[11, 14] = 1;
+                    break;
+
+                case '2':
+                    mezo[30, 31] = 1;
+                    mezo[31, 31] = 1;
+                    mezo[32, 31] = 1;
+                    mezo[32, 32] = 1;
+                    mezo[33, 32] = 1;
+                    mezo[33, 33] = 1;
+                    mezo[34, 33] = 1;
+                    mezo[34, 34] = 1;
+                    break;
+
+            }
+
         }
+
+
+        
+
 
     }
 }
