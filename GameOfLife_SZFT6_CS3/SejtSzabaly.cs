243ec749e31f41dcbf09394f8813687fa94e980d
Erdei Liliana <lilimerdei@gmail.com>
2023-10-29 23:24:43 +0100

    sejtszabaly

--- GameOfLife_SZFT6_CS3/SejtSzabaly.cs
+++ GameOfLife_SZFT6_CS3/SejtSzabaly.cs
@@ -16,30 +16,45 @@ namespace GameOfLife_SZFT6_CS3
         {
             Console.Clear();
 
-            // Alakzat alapján a mező felépítve
-            for (int sor = 0; sor < 75; sor++)
+
+
+
+            // Először összefűzzük az értékeket egy szövegbe, hogy egyszerre jelenítse meg az értékeket.
+
+            string mezokEgyben = "";
+
+            for (int sor = 0; sor < eletter.mezo.GetLength(0); sor++)
             {
-                for (int oszlop = 0; oszlop < 75; oszlop++)
+                for (int oszlop = 0; oszlop < eletter.mezo.GetLength(1); oszlop++)
                 {
-                    Console.Write(eletter.mezo[sor, oszlop] == 1 ? "█" : ".");
+                    mezokEgyben += eletter.mezo[sor, oszlop] ? "1" : "0";
                 }
-                Console.WriteLine();
+                mezokEgyben += Environment.NewLine;
             }
+            Console.WriteLine(mezokEgyben);
 
             // Pár sort kihagyunk, hogy a számokat is normálisan láthassuk
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine();
 
-            // Számok alapján a mező felépítve
+
+            // Változott mező ábrákkal való megjelenítés alapján 
+            mezokEgyben = "";
+
             for (int sor = 0; sor < eletter.mezo.GetLength(0); sor++)
             {
                 for (int oszlop = 0; oszlop < eletter.mezo.GetLength(1); oszlop++)
                 {
-                    Console.Write(eletter.mezo[sor, oszlop] == 1 ? "1" : "0");
+                    mezokEgyben += eletter.mezo[sor, oszlop] ? "█" : ".";
                 }
-                Console.WriteLine();
+                mezokEgyben += Environment.NewLine; // Egy sor után sortörés, hogy a következő sort ne a legelső sorba jelenítse meg.
             }
+
+            // Most jelenítjük meg az összes mezőt egyben ábrákkal.
+            Console.WriteLine(mezokEgyben);
+
+            Thread.Sleep(1000);
         }
 
 
@@ -51,79 +66,111 @@ namespace GameOfLife_SZFT6_CS3
             Console.Clear();
 
             // Az új mező, amit a számítások alapján feltöltünk
-            int[,] SegedMezo = new int[75, 75];
+
 
             for (int sor = 0; sor < eletter.mezo.GetLength(0); sor++)
             {
                 for (int oszlop = 0; oszlop < eletter.mezo.GetLength(1); oszlop++)
                 {
-                    int sejtvaltozas = 0; // --> Ebben tároljuk a számított értéket
+                    int sejtvaltozas = 0; // Ebben tároljuk a számított értéket
 
                     if (sor > 0)
                     {
                         if (oszlop > 0)
                         {
-                            sejtvaltozas += eletter.mezo[sor - 1, oszlop - 1] == 1 ? 1 : 0;
+                            sejtvaltozas += eletter.mezo[sor - 1, oszlop - 1] ? 1 : 0; // Bal felső szomszéd vizsgálata
                         }
-                        sejtvaltozas += eletter.mezo[sor - 1, oszlop] == 1 ? 1 : 0;
+                        sejtvaltozas += eletter.mezo[sor - 1, oszlop] ? 1 : 0; // Felső szomszéd
                         if (oszlop < eletter.mezo.GetLength(1) - 1)
                         {
-                            sejtvaltozas += eletter.mezo[sor - 1, oszlop + 1] == 1 ? 1 : 0;
+                            sejtvaltozas += eletter.mezo[sor - 1, oszlop + 1] ? 1 : 0; // Jobb felső szomszéd vizsgálata
                         }
                     }
                     if (oszlop > 0)
                     {
-                        sejtvaltozas += eletter.mezo[sor, oszlop - 1] == 1 ? 1 : 0;
+                        sejtvaltozas += eletter.mezo[sor, oszlop - 1] ? 1 : 0; // Bal szomszéd vizsgálata
                     }
                     if (oszlop < eletter.mezo.GetLength(1) - 1)
                     {
-                        sejtvaltozas += eletter.mezo[sor, oszlop + 1] == 1 ? 1 : 0;
+                        sejtvaltozas += eletter.mezo[sor, oszlop + 1] ? 1 : 0; // Jobb szomszéd vizsgálata
                     }
                     if (sor < eletter.mezo.GetLength(0) - 1)
                     {
                         if (oszlop > 0)
                         {
-                            sejtvaltozas += eletter.mezo[sor + 1, oszlop - 1] == 1 ? 1 : 0;
+                            sejtvaltozas += eletter.mezo[sor + 1, oszlop - 1] ? 1 : 0; // Bal alsó szomszéd vizsgálata
                         }
-                        sejtvaltozas += eletter.mezo[sor + 1, oszlop] == 1 ? 1 : 0;
+                        sejtvaltozas += eletter.mezo[sor + 1, oszlop] ? 1 : 0; // Alsó szomszéd vizsgálata
                         if (oszlop < eletter.mezo.GetLength(1) - 1)
                         {
-                            sejtvaltozas += eletter.mezo[sor + 1, oszlop + 1] == 1 ? 1 : 0;
+                            sejtvaltozas += eletter.mezo[sor + 1, oszlop + 1] ? 1 : 0; // Jobb alsó szomszéd vizsgálata
                         }
                     }
 
-                    // a számított értéket egy segéd mezőbe tesszük bele
-                    SegedMezo[sor, oszlop] = sejtvaltozas;
+                    if (eletter.mezo[sor, oszlop])
+                    {
+                        if (sejtvaltozas == 2 || sejtvaltozas == 3)
+                        {
+                            eletter.mezo[sor, oszlop] = true;
+                        }
+                        else
+                        {
+                            eletter.mezo[sor, oszlop] = false; // A cella meghal
+                        }
+                    }
+                    else
+                    {
+                        if (sejtvaltozas == 3)
+                        {
+                            eletter.mezo[sor, oszlop] = true; // A cella újjászületik
+                        }
+                    }
+
                 }
             }
 
-            // Az új mező tartalmát másoljuk vissza az eredeti mezőbe
-            Array.Copy(SegedMezo, eletter.mezo, SegedMezo.Length);
 
 
 
 
-            // Változott mező alapkzat alapján kiépítve
+
+            // Itt is összefűzzük az értékeket, hogy egyszerre legyen megjelenítve a console applikációban
+
+            string mezokEgyben = "";
+
             for (int sor = 0; sor < eletter.mezo.GetLength(0); sor++)
             {
                 for (int oszlop = 0; oszlop < eletter.mezo.GetLength(1); oszlop++)
                 {
-                    Console.Write(eletter.mezo[sor, oszlop] == 1 ? "█" : ".");
+                    mezokEgyben += eletter.mezo[sor, oszlop] ? "1" : "0";
                 }
-                Console.WriteLine();
+                mezokEgyben += Environment.NewLine; // Egy sor után sortörés, hogy a következő sort ne a legelső sorba jelenítse meg.
             }
+            Console.WriteLine(mezokEgyben);
+
+            // Pár sort kihagyunk itt is, hogy az ábrákat is normálisan láthassuk
             Console.WriteLine();
-            // Változott mező számokkal való megjelenítés alapján 
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            // Változott mező ábrákkal való megjelenítés alapján 
+            mezokEgyben = "";
+
             for (int sor = 0; sor < eletter.mezo.GetLength(0); sor++)
             {
                 for (int oszlop = 0; oszlop < eletter.mezo.GetLength(1); oszlop++)
                 {
-                    Console.Write(eletter.mezo[sor, oszlop] == 1 ? "1" : "0");
+                    mezokEgyben += eletter.mezo[sor, oszlop] ? "█" : ".";
                 }
-                Console.WriteLine();
+                mezokEgyben += Environment.NewLine; // Egy sor után sortörés, hogy a következő sort ne a legelső sorba jelenítse meg.
             }
 
+            // Most jelenítjük meg az összes mezőt egyben ábrákkal.
+            Console.WriteLine(mezokEgyben);
 
+            //Itt egy időzítő, hogy mikor menjen a sejtváltozás következő köre
+            Thread.Sleep(1000);
 
 
         }
